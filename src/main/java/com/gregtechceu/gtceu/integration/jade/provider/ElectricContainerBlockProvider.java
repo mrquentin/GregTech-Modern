c806772ed194a0657236b11d8c23fb8a682be986
MrQuentin <32539717+mrquentin@users.noreply.github.com>
2025-04-12 18:18:45 -0400

    first test

--- src/main/java/com/gregtechceu/gtceu/integration/jade/provider/ElectricContainerBlockProvider.java
+++ src/main/java/com/gregtechceu/gtceu/integration/jade/provider/ElectricContainerBlockProvider.java
@@ -3,6 +3,8 @@ package com.gregtechceu.gtceu.integration.jade.provider;
 import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.api.capability.GTCapabilityHelper;
 import com.gregtechceu.gtceu.api.capability.IEnergyContainer;
+import com.gregtechceu.gtceu.api.capability.IEnergyInfoProvider;
+import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
 import com.gregtechceu.gtceu.utils.FormattingUtil;
 
 import net.minecraft.Util;
@@ -10,55 +12,105 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 
 import org.jetbrains.annotations.Nullable;
 import snownee.jade.api.BlockAccessor;
+import snownee.jade.api.IBlockComponentProvider;
+import snownee.jade.api.IServerDataProvider;
 import snownee.jade.api.ITooltip;
 import snownee.jade.api.config.IPluginConfig;
 import snownee.jade.api.ui.BoxStyle;
 
-public class ElectricContainerBlockProvider extends CapabilityBlockProvider<IEnergyContainer> {
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
 
-    public ElectricContainerBlockProvider() {
-        super(GTCEu.id("electric_container_provider"));
-    }
-
-    @Nullable
-    @Override
-    protected IEnergyContainer getCapability(Level level, BlockPos pos, @Nullable Direction side) {
-        return GTCapabilityHelper.getEnergyContainer(level, pos, side);
-    }
+public class ElectricContainerBlockProvider implements IBlockComponentProvider, IServerDataProvider<BlockAccessor> {
 
     @Override
-    protected void write(CompoundTag data, IEnergyContainer capability) {
-        data.putLong("Energy", capability.getEnergyStored());
-        data.putLong("MaxEnergy", capability.getEnergyCapacity());
+    public ResourceLocation getUid() {
+        return GTCEu.id("electric_container_provider");
     }
 
     @Override
-    protected void addTooltip(CompoundTag capData, ITooltip tooltip, Player player, BlockAccessor block,
-                              BlockEntity blockEntity, IPluginConfig config) {
-        long maxStorage = capData.getLong("MaxEnergy");
-        if (maxStorage == 0) return; // do not add empty max storage progress bar
+    public void appendServerData(CompoundTag compoundTag, BlockAccessor blockAccessor) {
+        if (blockAccessor.getBlockEntity() instanceof IMachineBlockEntity blockEntity) {
+            var machine = blockEntity.getMetaMachine();
+            if (machine instanceof IEnergyInfoProvider energyInfoProvider) {
+                var supportBigIntegers = energyInfoProvider.supportsBigIntEnergyValues();
+                compoundTag.putBoolean("SupportBigIntegers", supportBigIntegers);
 
-        long stored = capData.getLong("Energy");
-        var helper = tooltip.getElementHelper();
+                var energyInfo = energyInfoProvider.getEnergyInfo();
+                if (!supportBigIntegers) {
+                    compoundTag.putLong("Energy", energyInfo.stored().longValue());
+                    compoundTag.putLong("MaxEnergy", energyInfo.capacity().longValue());
+                } else {
 
-        tooltip.add(
-                helper.progress(
-                        getProgress(stored, maxStorage),
-                        Component.translatable("gtceu.jade.energy_stored", FormattingUtil.formatNumbers(stored),
-                                FormattingUtil.formatNumbers(maxStorage)),
-                        helper.progressStyle().color(0xFFEEE600, 0xFFEEE600).textColor(-1),
-                        Util.make(BoxStyle.DEFAULT, style -> style.borderColor = 0xFF555555),
-                        true));
+                    compoundTag.putByteArray("Energy", energyInfo.stored().toByteArray());
+                    compoundTag.putByteArray("MaxEnergy", energyInfo.capacity().toByteArray());
+                }
+            }
+        }
     }
 
     @Override
-    protected boolean allowDisplaying(IEnergyContainer capability) {
-        return !capability.isOneProbeHidden();
+    public void appendTooltip(ITooltip iTooltip, BlockAccessor blockAccessor, IPluginConfig iPluginConfig) {
+        if (blockAccessor.getBlockEntity() instanceof IMachineBlockEntity blockEntity) {
+            var machine = blockEntity.getMetaMachine();
+            if (machine instanceof IEnergyInfoProvider energyInfoProvider) {
+                var supportBigIntegers = blockAccessor.getServerData().getBoolean("SupportBigIntegers");
+
+                String energyStr;
+                String maxEnergyStr;
+                float progress;
+
+                if (supportBigIntegers) {
+                    var energy = new BigInteger(blockAccessor.getServerData().getByteArray("Energy"));
+                    var maxEnergy = new BigInteger(blockAccessor.getServerData().getByteArray("MaxEnergy"));
+                    if (maxEnergy.compareTo(BigInteger.ZERO) <= 0) return;
+                    energyStr = FormattingUtil.formatNumbers(energy);
+                    maxEnergyStr = FormattingUtil.formatNumbers(maxEnergy);
+                    progress = getProgress(energy, maxEnergy);
+                } else {
+                    var energy = blockAccessor.getServerData().getLong("Energy");
+                    var maxEnergy = blockAccessor.getServerData().getLong("MaxEnergy");
+                    if (maxEnergy == 0) return;
+                    energyStr = FormattingUtil.formatNumbers(energy);
+                    maxEnergyStr = FormattingUtil.formatNumbers(maxEnergy);
+                    progress = getProgress(energy, maxEnergy);
+                }
+
+                var helper = iTooltip.getElementHelper();
+
+                iTooltip.add(
+                        helper.progress(
+                                progress,
+                                Component.translatable("gtceu.jade.energy_stored", energyStr, maxEnergyStr),
+                                helper.progressStyle().color(0xFFEEE600, 0xFFEEE600).textColor(-1),
+                                Util.make(BoxStyle.DEFAULT, style -> style.borderColor = 0xFF555555),
+                                true
+                        )
+                );
+            }
+        }
     }
+
+    protected float getProgress(long progress, long maxProgress) {
+        return maxProgress == 0 ? 0 : (float) ((double) progress / maxProgress);
+    }
+
+    protected float getProgress(BigInteger progress, BigInteger maxProgress) {
+        if (maxProgress.equals(BigInteger.ZERO)) return 0;
+        return new BigDecimal(progress).divide(new BigDecimal(maxProgress), MathContext.DECIMAL32).floatValue();
+    }
+
+//    @Override
+//    protected boolean allowDisplaying(IEnergyContainer capability) {
+//        return !capability.isOneProbeHidden();
+//    }
 }
